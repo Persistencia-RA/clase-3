5ace57a16d2dc1f7adb270431b531ba722f546ba
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-04-20 14:34:52 -0300

    se cambia nombre base

--- Clase-4/1-insertar-actualizar.js
+++ Clase-4/1-insertar-actualizar.js
@@ -1,49 +1,49 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("prueba", "root", "root", {
-    host: "localhost",
-    dialect: "mariadb",
-    port: 3310 /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+const sequelize = new Sequelize('persistencia', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mariadb',
+  port: 3310 /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
 });
 
 sequelize
-    .authenticate()
-    .then(() => {
-        console.log("Connection has been established successfully.");
-    })
-    .catch((err) => {
-        console.error("Unable to connect to the database:", err);
-    });
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 class Users extends Sequelize.Model {}
 Users.init(
-    {
-        firstName: Sequelize.STRING,
-        lastName: Sequelize.STRING,
-    },
-    { sequelize, modelName: "users" }
+  {
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+  },
+  { sequelize, modelName: 'users' },
 );
 
 sequelize
-    .sync()
-    .then(() =>
-        Users.create({
-            firstName: "Pedro",
-            lastName: "Rodriguez",
-        })
-    )
-    .then((jane) => {
-        console.log(jane.toJSON());
-    })
-    .then(() =>
-        Users.update(
-            { firstName: "Juan" },
-            {
-                where: {
-                    lastName: "Rodriguez",
-                },
-            }
-        ).then(() => {
-            console.log("Done");
-        })
-    );
+  .sync()
+  .then(() =>
+    Users.create({
+      firstName: 'Pedro',
+      lastName: 'Rodriguez',
+    }),
+  )
+  .then((jane) => {
+    console.log(jane.toJSON());
+  })
+  .then(() =>
+    Users.update(
+      { firstName: 'Juan' },
+      {
+        where: {
+          lastName: 'Rodriguez',
+        },
+      },
+    ).then(() => {
+      console.log('Done');
+    }),
+  );
