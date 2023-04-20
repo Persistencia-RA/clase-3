80242c123d09e7e11159390de88ec6e3dde3b2d8
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-04-20 00:08:44 -0300

    Se agregan dependecias

--- Clase-3/nro_1_model_users.js
+++ Clase-3/nro_1_model_users.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(`prueba`, "root", "root", {
+const sequelize = new Sequelize("prueba", "root", "root", {
   host: "localhost",
   dialect: "mariadb",
   port: 3310 /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
