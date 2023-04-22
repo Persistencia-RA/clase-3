5ba6d0c2c04fbc4ce3ffa275dcb0eb8446836b87
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-04-22 12:00:59 -0300

    m

---
+++ Clase-4/funciones.js
@@ -0,0 +1,26 @@
+export function crearUsuario(usuario, nombre, apellido) {
+  return usuario.create({
+    firstName: nombre,
+    lastName: apellido,
+  });
+}
+
+export function actualizarNombrePorId(usuario, nombreNuevo, idUsuario) {
+  return usuario.update(
+    { firstName: nombreNuevo },
+    { where: { id: idUsuario } },
+  );
+}
+
+export function actualizarNombrePorApellido(usuario, nombreNuevo, apellido) {
+  return usuario.update(
+    { firstName: nombreNuevo },
+    { where: { lastName: apellido } },
+  );
+}
+
+export function eliminarPorId(usuario, idNumero) {
+  return usuario.destroy({
+    where: { id: idNumero },
+  });
+}
